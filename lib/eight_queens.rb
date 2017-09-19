222532fba36fc51983966add7dfefe231c779a88
John Marks <johnjmarks4@gmail.com>
2017-09-19 00:57:05 -0500

    Add rudimentary rec_search method

--- lib/eight_queens.rb
+++ lib/eight_queens.rb
@@ -1,67 +1,114 @@
 require_relative 'chess_board'
 require_relative 'queen'
 
-board = ChessBoard.new
+class Game
+  attr_accessor :board
 
-public
-
-def permutations_strategy
-  queens = 0
-  (0..7).to_a.permutation.to_a.shuffle.each do |ary|
-    ary.each_with_index do |num, i|
-      board[i][num] = Queen.new(i, num)
-      queens += 1
-    end
-    if queens == 8 && checked? == false
-      show
-      return board 
-    else
-      queens = 0
-      board.each { |row| row.map! { |square| square = " " } }
-    end
+  def initialize
+    cb = ChessBoard.new
+    @queens = []
+    @board_obj = cb
+    @board = cb.board
+    @s_row = @board[0]
   end
-end
 
-def checked?
-  board.each do |row|
-    row.each do |square|
-      if square.class == Queen
-        # descending-right: 
-        r = square.row + 1
-        c = square.column + 1
-        while r <= 7 && c <= 7
-          return true if board[r][c].class == Queen
-          c += 1
-          r += 1
-        end
-        # ascending-left:
-        r = square.row + 1
-        c = square.column - 1
-        while r <= 7 && c >= 0
-          return true if board[r][c].class == Queen
-          c -= 1
-          r += 1
-        end
-        # descending-left:
-        r = square.row - 1
-        c = square.column - 1
-        while r >= 0 && c >= 0
-          return true if board[r][c].class == Queen
-          c -= 1
-          r -= 1
-        end
-        # descending-right]
-        r = square.row - 1
-        c = square.column + 1
-        while r >= 0 && c <= 7
-          return true if board[r][c].class == Queen
-          c += 1
-          r -= 1
-        end
+  def print_board
+    @board_obj.show
+  end
+
+  def permutations_strategy
+    @queens = []
+    (0..7).to_a.permutation.to_a.shuffle.each do |ary|
+      ary.each_with_index do |num, i|
+        @board[i][num] = Queen.new(i, num, self)
+        @queens << @board[i][num]
+      end
+      if @queens.length == 8 && in_check? == false
+        print_board
+        return @board 
+      else
+        @queens = []
+        @board.each { |row| row.map! { |square| square = " " } }
       end
     end
   end
-  return false
+
+  def start
+    c = 7
+    @r = 0
+    while @r < 7
+      rec_search(c)
+    end
+  end
+
+  def rec_search(c)
+    if c < 0
+      @s_row = @board[@r -= 1]
+    else
+      @s_row[c] = Queen.new(@r, c, self)
+      @board[@r] = @s_row
+      if in_check?
+        @board[@r][c] = " "
+        @s_row = @board[@r]
+        rec_search(c - 1)
+      else
+        @queens << @s_row[c]
+        @s_row = @board[@r += 1]
+      end
+    end
+  end
+
+  def perm_in_check?
+    @board.each do |row|
+      row.each do |square|
+        if square.class == Queen
+          # descending-right: 
+          r = square.r + 1
+          c = square.c + 1
+          while r <= 7 && c <= 7
+            return true if board[r][c].class == Queen
+            c += 1
+            r += 1
+          end
+          # ascending-left:
+          r = square.r + 1
+          c = square.c - 1
+          while r <= 7 && c >= 0
+            return true if board[r][c].class == Queen
+            c -= 1
+            r += 1
+          end
+          # descending-left:
+          r = square.r - 1
+          c = square.c - 1
+          while r >= 0 && c >= 0
+            return true if board[r][c].class == Queen
+            c -= 1
+            r -= 1
+          end
+          # descending-right]
+          r = square.r - 1
+          c = square.c + 1
+          while r >= 0 && c <= 7
+            return true if board[r][c].class == Queen
+            c += 1
+            r -= 1
+          end
+        end
+      end
+    end
+    return false
+  end
+
+  def in_check?
+    @queens.each do |q|
+      return true if q.can_take_piece? == true
+    end
+    false
+  end
 end
 
-board.permutations_strategy
+game = Game.new
+#game.permutations_strategy
+game.start
+game.print_board
--- lib/queen.rb
+++ lib/queen.rb
@@ -1,10 +1,88 @@
 class Queen
-  attr_accessor :row, :column, :unicode
+  attr_accessor :r, :c, :unicode
 
-  def initialize(row, column)
+  def initialize(row, column, game)
+    @game = game
+    @r = row
+    @c = column
     @unicode = "\u265B" #display color as white
     # @unicode = "\u2655" #display color as black
-    @row = row
-    @column = column
+  end
+
+  def can_take_piece?
+    moves = [asc_right, asc_left, desc_left, desc_right, up, down, right, left]
+    moves.any? { |m| m == true }
+  end
+
+  def asc_right
+    r = @r + 1
+    c = @c + 1
+    while r <= 7 && c <= 7
+      return true if @game.board[r][c].is_a?(Queen)
+      c += 1
+      r += 1
+    end
+  end
+
+  def asc_left
+    r = @r + 1
+    c = @c - 1
+    while r <= 7 && c >= 0
+      return true if @game.board[r][c].is_a?(Queen)
+      c -= 1
+      r += 1
+    end
+  end
+
+  def desc_left
+    r = @r - 1
+    c = @c - 1
+    while r >= 0 && c >= 0
+      return true if @game.board[r][c].is_a?(Queen)
+      c -= 1
+      r -= 1
+    end
+  end
+
+  def desc_right
+    r = @r - 1
+    c = @c + 1
+    while r >= 0 && c <= 7
+      return true if @game.board[r][c].is_a?(Queen)
+      c += 1
+      r -= 1
+    end
+  end
+
+  def right
+    c = @c + 1
+    while c <= 7
+      return true if @game.board[@r][c].is_a?(Queen)
+      c += 1
+    end
+  end
+
+  def left
+    c = @c - 1
+    while c >= 0
+      return true if @game.board[@r][c].is_a?(Queen)
+      c -= 1
+    end
+  end
+
+  def up
+    r = @r + 1
+    while r <= 7
+      return true if @game.board[r][@c].is_a?(Queen)
+      r += 1
+    end
+  end
+
+  def down
+    r = @r - 1
+    while r >= 0
+      return true if @game.board[r][@c].is_a?(Queen)
+      r -= 1
+    end
   end
 end
