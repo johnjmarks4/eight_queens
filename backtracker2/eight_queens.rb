96a5c00a77ff76c48052a22861a1b5a60139ca14
John Marks <johnjmarks4@gmail.com>
2017-09-21 20:53:02 -0500

    Fix search algorithm

--- backtracker2/eight_queens.rb
+++ backtracker2/eight_queens.rb
@@ -1,65 +1,55 @@
-require_relative 'board'
-require_relative 'queen'
-
 class Game
-  attr_accessor :queens
-
-  def initialize
-    @board_obj = Board.new
-    @board = @board_obj.board
-    @queens = []
-
-    #@board[7][1] = Queen.new(7, 1)
-    #@queens << Queen.new(7, 1)
-    #@board[6][1] = Queen.new(6, 1)
-    #@queens << Queen.new(6, 1)
-  end
 
   def solution
+    @queens = []
     r = 7
-    #while @r > 0
-    2.times do
-      success = search(r)
-      if success == true
-        r -= 1
-      else
-        r += 1
-      end
-    end
+    search(r)
+    @queens
   end
 
   def search(r)
-    8.times do |c|
-      @board[r][c] = Queen.new(r, c)
-      @queens << Queen.new(r, c)
-      if in_check? != true
-        return true
+    if r == 0
+      return
+    else
+      7.times do |i|
+        @queens << [r, i]
+        if in_check? == false
+          break # not return, right?
+        else
+          @queens.pop
+        end
       end
+      search(r -= 1)
     end
-    false
-  end
-
-  def print_board
-    @board_obj.show
   end
 
   def in_check?
-    queen = @queens.last
-    if queen.in_check?(@queens)
-      remove(queen)
-      true
-    else
-      false
+    sums = []
+    @queens.each do |pos|
+      sums << pos[1]
     end
-  end
+    if sums.length != sums.uniq.length
+      return true
+    end
 
-  def remove(queen)
-    @queens.pop
-    @board[queen.r][queen.c] = " "
+    sums = []
+    @queens.each do |pos|
+      sums << (pos[0] + pos[1])
+    end
+    if sums.length != sums.uniq.length
+      return true
+    end
+
+    sums = []
+    @queens.each do |pos|
+      sums << (pos[0] - pos[1])
+    end
+    if sums.length != sums.uniq.length
+      return true
+    end
+    false
   end
 end
 
 game = Game.new
-game.print_board
-game.solution
-game.print_board
+print game.solution
