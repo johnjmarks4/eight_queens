b76260dd6ec0252e394d8ad602e379b66ee912af
John Marks <johnjmarks4@gmail.com>
2017-09-24 01:14:38 -0500

    Refactored in_check? method

--- permutations/eight_queens.rb
+++ permutations/eight_queens.rb
@@ -6,60 +6,33 @@ board = ChessBoard.new
 public
 
 def permutations_strategy
-  queens = 0
+  queens = []
   (0..7).to_a.permutation.to_a.shuffle.each do |ary|
     ary.each_with_index do |num, i|
       board[i][num] = Queen.new(i, num)
-      queens += 1
+      queens << [i, num]
     end
-    if queens == 8 && checked? == false
+    if queens.length == 8 && in_check?(queens) == false
       show
       return board 
     else
-      queens = 0
+      queens = []
       board.each { |row| row.map! { |square| square = " " } }
     end
   end
 end
 
-def checked?
-  board.each do |row|
-    row.each do |square|
-      if square.class == Queen
-        # descending-right: 
-        r = square.row + 1
-        c = square.column + 1
-        while r <= 7 && c <= 7
-          return true if board[r][c].class == Queen
-          c += 1
-          r += 1
-        end
-        # ascending-left:
-        r = square.row + 1
-        c = square.column - 1
-        while r <= 7 && c >= 0
-          return true if board[r][c].class == Queen
-          c -= 1
-          r += 1
-        end
-        # descending-left:
-        r = square.row - 1
-        c = square.column - 1
-        while r >= 0 && c >= 0
-          return true if board[r][c].class == Queen
-          c -= 1
-          r -= 1
-        end
-        # descending-right]
-        r = square.row - 1
-        c = square.column + 1
-        while r >= 0 && c <= 7
-          return true if board[r][c].class == Queen
-          c += 1
-          r -= 1
-        end
-      end
-    end
+def in_check?(queens)
+  cols, nw_diag, se_diag = [], [], []
+
+  queens.each do |pos|
+    cols << pos[1]
+    nw_diag << (pos[0] + pos[1])
+    se_diag << (pos[0] - pos[1])
+  end
+
+  [cols, nw_diag, se_diag].any? do |ary|
+    ary.length != ary.uniq.length
   end
   return false
 end
